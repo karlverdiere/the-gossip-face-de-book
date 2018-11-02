240ede1fc08e81c1fdc593f6470ace2869ff992e
karlverdiere <karl.verdiere@hotmail.fr>
2018-11-02 04:02:35 +0100

    quand git hub te supprime les route et ton css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root to: 'users#home'
+  resources :gossips
+  get 'users/home', to: 'users#home'
+   get 'users/login', to: 'users#login'
+   post 'users/login', to: 'users#check'
+   get 'users/deco', to: 'users#deco'
+   get 'users/deconnection', to: 'users#deconnection'
+    get 'users/new', to: 'users#new'
+   post 'users/create', to: 'users#create'
+   get 'users/profil', to: 'users#profil'
+    get 'users/index', to: 'users#index'
+  end
